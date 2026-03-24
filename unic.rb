39b0ed6f25cec7f19d045b68e32eeeb348675ddb
YoungJinJung <zerone@lguplus.co.kr>
2026-03-24 18:16:20 +0900

    modify binary

--- unic.rb
+++ unic.rb
@@ -1,7 +1,7 @@
 class Unic < Formula
   desc "Go-based TUI tool for browsing and managing AWS resources in the terminal"
   homepage "https://github.com/DevopsArtFactory/unic"
-  url "https://github.com/DevopsArtFactory/unic/releases/download/v0.0.1/unic-darwin-arm64"
+  url "https://github.com/DevopsArtFactory/unic/releases/download/v0.0.1/unic-darwin-amd64"
   version "v0.0.1"
   sha256 "e2c4fc700c7ebb6aa737ef7c38b0b019de43a96bf08ca338aef03c60e4018364"
 
